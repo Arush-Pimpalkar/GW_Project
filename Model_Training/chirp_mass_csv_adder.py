3fc66ae79824afe320aa7eae454cc18c7c0ad3b4
Arush-Pimpalkar <arush.pimpalkar@gmail.com>
2024-06-22 16:56:40 +0530

    data_pipeline

--- Model_Training/chirp_mass_csv_adder.py
+++ Model_Training/chirp_mass_csv_adder.py
@@ -1,30 +1,17 @@
-import re
 import csv
 
 
 def chirp_mass(m1, m2):
-    chirp_m = ((m1 * m2) ** (3 / 5)) / ((m1 + m2) ** (1 / 5))
-    return chirp_m
-
-# while True:
-#     m1 = input("enter m1: ")
-#     m2 = input("enter m2: ")
-
-#     chirp_m = chirp_mass(int(m1), int(m2))
-#     print(chirp_m)
-
-def get_chirp_masses(filename):
-    m1_match = re.search(r"m1=(\d+)", filename)
-    m2_match = re.search(r"m2=(\d+)", filename)
-    if m1_match and m2_match:
-        m1 = int(m1_match.group(1))
-        m2 = int(m2_match.group(1))
-        chirp_m = chirp_mass(m1, m2)
+    """Calculate the chirp mass of two masses."""
+    try:   
+        chirp_m = ((m1 * m2) ** (3 / 5)) / ((m1 + m2) ** (1 / 5))
         return chirp_m
-    return None
+    except:
+        return 0
 
 
-filename = "/home/arush/GW_Project_1/Data_Generation/Continous_Check/cont_data_test.csv"
+# Define the path to the CSV file
+filename = "/home/arush/GW_Project_1/Data_Generation/Continous_Check/cont_data_val.csv"
 
 # Read the CSV content
 with open(filename, "r") as infile:
@@ -37,12 +24,10 @@ if "ChirpMass" not in header:
     header.append("ChirpMass")
 
 for row in rows[1:]:
-    filenames = row[1]
-    chirp_m = get_chirp_masses(filenames)
-    if chirp_m is not None:
-        row.append(chirp_m)
-    else:
-        row.append("0.00")  # If chirp mass could not be calculated, append an empty string
+    m1 = int(row[1])
+    m2 = int(row[2])
+    chirp_m = chirp_mass(m1, m2)
+    row.append(f"{chirp_m:.2f}")
 
 # Write the updated content back to the same file
 with open(filename, "w", newline="") as outfile:
